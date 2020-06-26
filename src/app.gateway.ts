b449885debbb60efdb40002460d9757f4f4dc783
Benjamin Payoux <bpa@frianbiz.com>
2020-06-26 15:15:44 +0200

    wip

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -7,6 +7,7 @@ export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
     @WebSocketServer() server;
     players = [];
     cards = undefined;
+    board = {};
 
     async handleConnection(client: Socket): Promise<void> {
         this.players.push({
@@ -16,13 +17,14 @@ export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
         // Notify connected clients of new player list
         this.server.emit(`welcome`, this.players);
+        client.emit(`board`, this.board);
     }
 
     async handleDisconnect(client: Socket): Promise<void> {
         this.players = this.players.filter((player) => player.id !== client.id);
 
         // Notify connected clients of new player list
-        this.server.emit(`welcome`, this.players);
+        this.server.emit(`goodbye`, { players: this.players, board: this.board });
     }
 
     @SubscribeMessage('nick')
@@ -34,12 +36,14 @@ export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
     @SubscribeMessage('reset')
     async onReset(client: Socket): Promise<void> {
         this.cards = undefined;
+        this.board = {};
         this.server.emit('reset');
     }
 
     @SubscribeMessage('cards')
-    async onFrames(client: Socket, cards: string[]): Promise<void> {
+    async onCards(client: Socket, cards: string[]): Promise<void> {
         if (this.cards === undefined) {
+            this.board = {};
             this.cards = cards;
             this.cards.sort(() => Math.random() - 0.5);
         }
@@ -47,12 +51,15 @@ export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
     @SubscribeMessage('pick')
     async onPick(client: Socket): Promise<void> {
-        this.server.emit('picked', this.cards.shift());
+        const cardName = this.cards.shift();
+        this.board[cardName] = [100, 100];
+        this.server.emit('picked', cardName);
     }
 
     @SubscribeMessage('dragend')
-    async onDragEnd(client: Socket, data: Object): Promise<void> {
+    async onDragEnd(client: Socket, data: any): Promise<void> {
         console.log(data);
+        this.board[data.name] = [data.x, data.y];
         this.server.emit('dragend', data);
     }
 }
