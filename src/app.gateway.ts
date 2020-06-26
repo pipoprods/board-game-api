1dfcf65d6de36a5700a45624ebed64e26a4202c3
Benjamin Payoux <bpa@frianbiz.com>
2020-06-26 14:00:55 +0200

    wip

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -6,6 +6,7 @@ export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
     @WebSocketServer() server;
     players = [];
+    cards = undefined;
 
     async handleConnection(client: Socket): Promise<void> {
         this.players.push({
@@ -28,5 +29,30 @@ export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
     async onNick(client: Socket, nick: string): Promise<void> {
         this.players.find(player => player.id === client.id).nick = nick;
         this.server.emit('welcome', this.players);
+    }
+
+    @SubscribeMessage('reset')
+    async onReset(client: Socket): Promise<void> {
+        this.cards = undefined;
+        this.server.emit('reset');
+    }
+
+    @SubscribeMessage('cards')
+    async onFrames(client: Socket, cards: string[]): Promise<void> {
+        if (this.cards === undefined) {
+            this.cards = cards;
+            this.cards.sort(() => Math.random() - 0.5);
+        }
+    }
+
+    @SubscribeMessage('pick')
+    async onPick(client: Socket): Promise<void> {
+        this.server.emit('picked', this.cards.shift());
+    }
+
+    @SubscribeMessage('dragend')
+    async onDragEnd(client: Socket, data: Object): Promise<void> {
+        console.log(data);
+        this.server.emit('dragend', data);
     }
 }
